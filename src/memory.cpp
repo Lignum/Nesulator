cfcb33a705560e030c96becf16c16b9bb9ef9edd
Lignum <me@lignum.pw>
2018-04-21 16:12:01 +0200

    Oops

--- src/memory.cpp
+++ src/memory.cpp
@@ -24,7 +24,6 @@ uint8_t Memory::read(MemoryAccessSource source, Address address) const {
         case MemoryAccessSource::CPU: return readCPU(address);
         case MemoryAccessSource::PPU: return readPPU(address);
     }
-
 }
 
 void Memory::write(MemoryAccessSource source, Address address, uint8_t value) {
